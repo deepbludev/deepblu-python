544b1f1809595e7eea5fa310b57b6ea71e2442db
Carlo Casorzo <carlo@deepblu.dev>
2023-01-31 19:23:13 +0100

    docs: update result module docs

--- deepblu/result/__init__.py
+++ deepblu/result/__init__.py
@@ -1,21 +1,13 @@
 from functools import wraps
-from typing import (
-    Any,
-    Callable,
-    Generic,
-    Optional,
-    ParamSpec,
-    Type,
-    TypeAlias,
-    TypeVar,
-    cast,
-)
+from typing import Any, Callable, Generic, Optional, ParamSpec, TypeVar, cast
 
 TValue = TypeVar("TValue")
 TError = TypeVar("TError", bound=Exception)
 
 
 class Result(Generic[TValue, TError]):
+    """Represents a monadic result of a function that can be either ok or error."""
+
     __slots__ = ("__value", "__error", "__is_ok")
     __value: Optional[TValue]
     __error: Optional[TError]
@@ -34,6 +26,11 @@ class Result(Generic[TValue, TError]):
         return f"Ok({self.__value})" if self.is_ok else f"Error({self.__error})"
 
     def __eq_result__(self, other: "Result[TValue, TError]") -> bool:
+        """Checks if the result is equal to another result.
+
+        Used internally by __eq__.
+        Equality is checked by comparing the value and the error.
+        """
         is_equal_error = (
             self.error is not None
             and other.error is not None
@@ -45,47 +42,73 @@ class Result(Generic[TValue, TError]):
         return is_equal_value and is_equal_error
 
     def __eq__(self, other: Any) -> bool:
+        """Checks if the result is equal to another result or a value.
+
+        Equality is checked by comparing the value and the error.
+        """
         return isinstance(other, Result) and self.__eq_result__(
             cast(Result[TValue, Any], other)
         )
 
     @property
     def value(self) -> Optional[TValue]:
+        """Returns the value of the result.
+
+        If the result is an error, it will return None.
+        """
         return self.__value
 
     @property
     def error(self) -> Optional[TError]:
+        """Returns the error of the result.
+
+        If the result is ok, it will return None."""
         return self.__error
 
     @property
     def is_ok(self) -> bool:
+        """Returns True if the result is ok, False if it is an error."""
         return self.__is_ok
 
     @property
     def is_error(self) -> bool:
+        """Returns True if the result is an error, False if it is ok."""
         return not self.is_ok
 
     @classmethod
     def ok(cls, value: Optional[TValue] = None) -> "Result[TValue,Any]":
+        """Creates an ok result with the given value."""
         return cls(value=value, error=None)
 
     @classmethod
     def err(cls, error: Optional[TError] = None) -> "Result[Any, TError]":
+        """Creates an error result with the given error."""
         return cls(value=None, is_ok=False, error=error)
 
 
 def ok(value: Optional[TValue] = None) -> Result[TValue, Any]:
+    """Creates an ok result."""
     return Result.ok(value)
 
 
 def error(error: Optional[TError] | str = None) -> Result[Any, TError | Exception]:
+    """Creates an error result.
+
+    If the error is a string, it will be converted to an Exception.
+    """
     return Result.err(Exception(error) if isinstance(error, str) else error)
 
 
+# Used for getting the type of kwargs in a monadic function
 P = ParamSpec("P")
 
 
 def monadic(func: Callable[P, TValue]) -> Callable[P, Result[TValue, Any]]:
+    """Decorator for monadic functions.
+
+    Converts a function that can raise an exception into a function that returns a Result.
+    """
+
     @wraps(func)
     def decorator(*args: P.args, **kwargs: P.kwargs) -> Result[TValue, Any]:
         try:
