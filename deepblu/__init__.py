3e042241f62fda8ec75587e52e3a4f81af354f85
Carlo Casorzo <carlo@deepblu.dev>
2023-01-31 08:04:16 +0100

    feat: implement monadic result (#3)
    - feat: implement Result class, ok(), error()
    - feat: implement @monadic function decorator

--- deepblu/__init__.py
+++ deepblu/__init__.py
@@ -6,5 +6,35 @@ Library for advanced architectures/patterns in modern Python.
 Domain-Driven Design (DDD), CQRS, Event Sourcing, Event-driven architectures, message queues, unit of work, dependency injection and more.
 """
 
+from .di import (
+    Module,
+    add,
+    bind,
+    get,
+    inject,
+    injectable,
+    module,
+    provide_many,
+    registry,
+)
+from .result import Result, error, monadic, ok
 
 __version__ = "0.2.0"
+
+__all__ = [
+    # di
+    "Module",
+    "add",
+    "bind",
+    "get",
+    "inject",
+    "injectable",
+    "module",
+    "provide_many",
+    "registry",
+    # result
+    "Result",
+    "error",
+    "monadic",
+    "ok",
+]
